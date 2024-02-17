7127f865c6accb3e682f0de383fd845949fba8fc
rohitprocode <rohit990797@gmail.com>
2024-02-17 13:58:34 +0530

    useReducer Hook with Switch Case

--- src/Components/UseReducerHook.jsx
+++ src/Components/UseReducerHook.jsx
@@ -1,45 +1,86 @@
-import React, { useReducer, useState } from 'react'
+// import React, { useReducer, useState } from 'react'
 
+// const initialState = 0;
+// const reducer = (state,action)=>{
+//     console.log(state,action)
+//     if(action.type === "Add"){
+//         return state + 1;
+//     } else if(action.type === "Substract"){
+//         return state - 1;
+//     }
+//     return state
+// }
 
+// function UseReducerHook() {
+//     const [state,dispatch] = useReducer(reducer,initialState)
+//   return (
+//     <div>
+//       <h1>This is a useReducer hook</h1>
+//       <p></p>
+//       <h2>Counter : {state}</h2>
+//       <button onClick={()=>dispatch({type : "Add"})}>Click to Add</button>
+//       <button onClick={()=>dispatch({type : "Substract"})}>Click to Substract</button>
+//     </div>
+//   )
+// }
 
+// export default UseReducerHook
 
-const initialState = 0;
-const reducer = (state,action)=>{
-    console.log(state,action)   
-    if(action.type === "Add"){
-        return state + 1;
-    } else if(action.type === "Substract"){
-        return state - 1;
-    }
-    return state
-}
+import React, { useReducer, useState } from "react";
+const initialCountervalue = 100;
+
+const reducerFunc = (state, action) => {
+  switch (action.type) {
+    case "Increment" :
+      return state + 10
+      break;
+    case "Decrement" : 
+    return state - 10
+    break;
+    case "Multiply" : 
+    return state * 10
+    break;
+    case "Division" : 
+    return state / 10
+  }
+};
 
 function UseReducerHook() {
-    // const [Count , setCount] = useState(0)
-
-    // const actionFunctionAdd = () =>{
-    //     setCount(Count + 1)
-    // }
-
-    // const actionFunctionLess = () =>{
-    //     setCount(Count - 1)
-    // }
-
-    const [state,dispatch] = useReducer(reducer,initialState)
-
+  const [state, dispatch] = useReducer(reducerFunc, initialCountervalue);
+  // const [Counter,setCounter] = useState(100)
   return (
     <div>
-      <h1>This is a useReducer hook</h1>
-      <p></p>
-      {/* <h2>Counter : {Count}</h2>
-      <button onClick={()=>actionFunctionAdd()}>Click to Add</button><br />
-      <button onClick={()=>actionFunctionLess()}>Click to Substract</button> */}
-
-      <h2>Counter : {state}</h2>
-      <button onClick={()=>dispatch({type : "Add"})}>Click to Add</button>
-      <button onClick={()=>dispatch({type : "Substract"})}>Click to Substract</button>
+      <h1>Counter : {state}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "Increment" });
+        }}
+      >
+        Click to 10+
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "Decrement" });
+        }}
+      >
+        Click to -10
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "Multiply" });
+        }}
+      >
+        Click to 10x
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "Division" });
+        }}
+      >
+        Click to /10
+      </button>
     </div>
-  )
+  );
 }
 
-export default UseReducerHook
+export default UseReducerHook;
