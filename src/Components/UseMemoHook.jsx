286a8e7623c6537a8437c7f7aec7b5a87897f854
rohitprocode <rohit990797@gmail.com>
2024-02-19 23:25:32 +0530

    useMemo

--- src/Components/UseMemoHook.jsx
+++ src/Components/UseMemoHook.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useMemo, useRef, useState } from "react";
+import { InitialItems } from "./SelectedItemUseMemo";
 
-function UseMemoHook() {    
+function UseMemoHook() {
+  const [Counter, setCounter] = useState(100);
+  const [items] = useState(InitialItems);
 
-    const MultiplyFunc = () =>{
-        const x = parseFloat(document.getElementById('FirstInput').value);
-        const y = parseFloat(document.getElementById('SecondInput').value);
-        const result = x * y;
-        document.getElementById('result').innerText = `Result: ${result}`;
-    }
+  const IncreFunc = () => {
+    setCounter(Counter + 1);
+  };
 
-  return (                                                 
+  const selectedItem = useMemo(() => items.find((item) => item.isSelected), [items]);
+
+  console.log("Component Rendered");
+  return (
     <div>
       <h1>This is a useMemo Hook</h1>
-      <p><strong>useMemo</strong> is a react hook that memoizes a value and returns the same value until any of the dependencies in the dependency array changes,until then it will recompute a new value.</p>
-      <input type="text" placeholder='Enter First Number' id='FirstInput'/><br />
-      <input type="text" placeholder='Enter Second Number' id='SecondInput'/><br />
-      <button onClick={MultiplyFunc} >Multiply</button>
-      <h2 id='result' >Your Output result will reflect here:</h2>
+      <p>
+        <strong>useMemo</strong> is a react hook that memoizes a value and
+        returns the same value until any of the dependencies in the dependency
+        array changes,until then it will recompute a new value.
+      </p>
+      <h2>{Counter}</h2>
+      <button onClick={IncreFunc}>Increment</button>
+      <h1>Selected Item:{selectedItem?.id}</h1>
     </div>
-  )
+  );
 }
 
-export default UseMemoHook
+export default UseMemoHook;
